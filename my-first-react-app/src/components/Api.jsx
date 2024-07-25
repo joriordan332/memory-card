3ba31b338ea078067b3c7d9f85eada711d6a66e4
Jonathan O'Riordan <jooriordan@hotmail.com>
2024-07-25 13:22:28 +0100

    Removed API and added images manually
    Downloaded images manually and added them into an array
    Array of images is initially mounted on the page and each time image is clicked the shuffleArray function randomises the images
    Each time image is clicked it's pushed into and then if an the image is clicked again after having already relicked the game is over and resets

--- my-first-react-app/src/components/Api.jsx
+++ my-first-react-app/src/components/Api.jsx
@@ -1,52 +1,83 @@
 import { useState } from "react";
-import Characters from './Characters';
-import simpsonsImage from '../img/thesimpsons.jpg';
+
+import bart from '../img/bart.png';
+import homer from '../img/homer.png';
+import lisa from '../img/lisa.png';
+import marge from '../img/marge.png';
+import abe from '../img/abe.png';
+import apu from '../img/apu.png';
+import milhouse from '../img/milhouse.png';
+import skinner from '../img/skinner.png';
+import ralph from '../img/ralph.png';
+import mrburns from '../img/mrburns.png';
+import wiggum from '../img/wiggum.png';
+import nelson from '../img/nelson.png';
 
 
-function App() {
-    const [characters, setCharacters] = useState(null);
-  
-    const getCharacters = async () => {
-      try {
-        const response = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=16');
-        
-          if (!response.ok) {
-              throw new Error('Error');
-          }
-          const data = await response.json();
-          console.log(data);
-          let charactersMap = data.map(item => {
-            return [item.character, item]
-          })
-          let characterMapArr = new Map(charactersMap);
-          let uniqueCharacters = [...characterMapArr.values()];
-          console.log(uniqueCharacters);
-          setCharacters(uniqueCharacters);
-      } catch(error) {
-        console.error('Error', error);
-      }
-      
-    };
-  
-    return (
-      <div className='App'>
-        <header className='App-header'>
-        
-          {characters ? (
-            <div>
-              <h2 className=''>Characters</h2>
-              <Characters characters={characters} setCharacters={setCharacters} />
-            </div>
-          ) : (
-            <div className='center-content'>
-              <h1>The Simpsons</h1>
-              <img src={simpsonsImage} className="img-prin" alt="the simpsons" />
-              <button className='btn' onClick={getCharacters}>See Characters</button>
-            </div>
-          )}
-        </header>
-      </div>
-    );
-  }
+const Images = [
+  bart,
+  homer,
+  lisa,
+  marge,
+  abe,
+  apu,
+  milhouse,
+  skinner,
+  ralph,
+  mrburns,
+  wiggum, 
+  nelson
+];
 
-  export default App
+const shuffleArray = (input) => {
+  // prevent sorting the original Array in place
+  return input.slice().sort(() => Math.random() - 0.5);
+};
+
+const Component = () => {
+  const [images, setImages] = useState(shuffleArray(Images));
+  const [clickedImages, setClickedImages] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  const incrementScore = (image) => {
+    if (clickedImages.includes(image)) {
+      alert('Game over')
+      setImages(shuffleArray(Images));
+      setClickedImages([]);
+      setScore(0);
+      return;
+    }
+
+    setScore(score + 1);
+
+    // update clicked images
+    setClickedImages(current => [...current, image]);
+    
+    if (score >= 12) {
+      alert('You win')
+      setScore(0);
+      // reset clicked images
+      setClickedImages([]);
+    }
+
+    setImages(shuffleArray(Images));
+    console.log(score);
+  };
+
+  return (
+    <div className="card">
+      {images.map((img) => (
+        <img
+          key={img}
+          src={img}
+          alt={img}
+          onClick={() => incrementScore(img)}
+        />
+      ))}
+    </div>
+  );
+};
+
+
+  export default Component
